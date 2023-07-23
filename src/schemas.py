607b85eaa81b943cf15d87ccc9912eb5b30a92cd
ProtKen <protcenko.kseniya@gmail.com>
2023-07-23 17:40:28 +0500

    feature: pydantic schemas for menu

---
+++ src/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, field_validator
+from typing import Optional
+
+
+class IdConverterMixin(BaseModel):
+    id: str
+
+    @field_validator('id', mode='before')
+    @classmethod
+    def convert_int_serial(cls, v):
+        if isinstance(v, int):
+            v = str(v)
+        return v
+
+
+class MenuCreationSchema(BaseModel):
+    title: str
+    description: str
+
+
+class MenuUpdateSchema(BaseModel):
+    title: Optional[str]
+    description: Optional[str]
+
+
+class MenuFullSchema(IdConverterMixin, MenuCreationSchema):
+    submenus_count: int
+    dishes_count: int
